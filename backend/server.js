308a96e7ac19c135366a46337648d908d56ee50f
Andres Varon <anvaron@gmail.com>
2025-01-23 15:53:24 -0500

    ENV file created

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
+//import dotenv from 'dotenv'; 
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
+//dotenv.config(); // Load environment variables from .env file
 
 // Middleware
 app.use(cors());
@@ -13,7 +16,7 @@ app.get('/api', (req, res) => {
     res.json({ message: 'Hello from the backend!' });
 });
 
-const PORT = 8888;
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+const port = 8888 || 3333;
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
 });
