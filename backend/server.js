8073db965b38a09c5aa1cdc2cbb13863175505a5
Andres Varon <anvaron@gmail.com>
2025-01-31 20:26:47 -0500

    basic server - ok

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,16 @@
-//import dotenv from 'dotenv'; 
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-
+dotenv.config();
 const app = express();
-//dotenv.config(); // Load environment variables from .env file
+const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Example API endpoint
-app.get('/api', (req, res) => {
-    res.json({ message: 'Hello from the backend!' });
-});
-
-const port = 8888 || 3333;
+// Start the server
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
